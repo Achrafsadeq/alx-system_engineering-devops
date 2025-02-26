0178b8896b77d584466ed4d10c7d6bbd92e55859
Achrafsadeq <achrafsadeqq@gmail.com>
2025-02-26 13:42:05 -0800

    Using strace, find out why Apache is returning a 500 error. Fix it and automate the fix using Puppet.

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Apache 500 error in WordPress by correcting a PHP file extension typo
+exec{'fix-wordpress':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/bin/:/bin/',
+}
